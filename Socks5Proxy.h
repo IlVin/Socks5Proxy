83c6ff8c975ffa2855052395c3f68a077efd4bde
Nate Riven <nightraven754@gmail.com>
2020-03-04 09:18:39 +0300

    fetch: connect to proxy by hostname. fetch: ability to disable debugging messages

--- Socks5Proxy.h
+++ Socks5Proxy.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <cctype>
-#include <client.h>
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include "IPAddress.h"
@@ -17,6 +16,7 @@
 #define SOCKS5_TIMEOUT 30000
 #endif
 
+//#define TRACE_MESSAGES
 
 class Socks5Proxy : public Client {
 private:
@@ -30,25 +30,37 @@ public:
         : client(client)
         , socks5Host(proxyIP)
         , socks5Port(proxyPort){
-    }  
+    }
+
+    Socks5Proxy (const char* host, uint16_t proxyPort, Client & client)
+        : client(client) 
+        , socks5Port(proxyPort) { WiFi.hostByName(host, socks5Host, _timeout); }
 
     virtual int connect(IPAddress destIp, uint16_t destPort){
+    #ifdef TRACE_MESSAGES
         Serial.println("Socks5Proxy: connect with destIP");
+    #endif
         int s5ConnectResult = client.connect(socks5Host, socks5Port);
         if (!s5ConnectResult){
             return s5ConnectResult;
         }
+    #ifdef TRACE_MESSAGES    
         Serial.println("Socks5Proxy: connected to Socks5");
+    #endif    
         return Socks5ConnectToDestination(destIp, destPort);
     }
 
     virtual int connect(const char* destHost, uint16_t destPort){
+    #ifdef TRACE_MESSAGES    
         Serial.println("Socks5Proxy: connect with destHost");
+    #endif    
         int s5ConnectResult = client.connect(socks5Host, socks5Port);
         if (!s5ConnectResult){
             return s5ConnectResult;
         }
+    #ifdef TRACE_MESSAGES    
         Serial.println("Socks5Proxy: connected to Socks5");
+    #endif    
         return Socks5ConnectToDestination(destHost, destPort);
     }
     virtual size_t write(uint8_t data){
@@ -127,25 +139,33 @@ protected:
     }
 
     bool Socks5Auth() {
+    #ifdef TRACE_MESSAGES    
         Serial.println("Socks5Auth");
+    #endif    
         struct TSocks5AuthRequest {
             uint8_t VER = 0x05;
             uint8_t NMETHODS = 0x01;
             uint8_t METHODS = 0x00;
         } __attribute__((packed)) s5AReq;
-        Serial.println("TSocks5AuthRequest:");
+    #ifdef TRACE_MESSAGES    
+        Serial.println("TSocks5AuthRequest:"); 
         for (size_t i = 0; i < sizeof(s5AReq); ++i) {
             Serial.printf("%d:0x%x(%d) ", i, ((uint8_t*)&s5AReq)[i], ((uint8_t*)&s5AReq)[i]);
         }
         Serial.println("");
 
         Serial.printf("VER: %d; NMETHODS: %d; METHODS: %d\n", s5AReq.VER, s5AReq.NMETHODS, s5AReq.METHODS);
+    #endif    
         if (WriteAll(client, (uint8_t*)&s5AReq, sizeof(TSocks5AuthRequest)) != sizeof(TSocks5AuthRequest)
             || !WaitClient(client)) {
+            #ifdef TRACE_MESSAGES    
                 Serial.println("Socks4Auth failed");
-            return false;
+            #endif    
+                return false;
         }
+    #ifdef TRACE_MESSAGES  
         Serial.println("Socks4Auth read response");
+    #endif    
         // Socks5 Auth Response
         struct TSocks5AuthResponse {
             uint8_t VER;
@@ -154,15 +174,21 @@ protected:
         if (ReadAll(client, (uint8_t*)&s5AResp, sizeof(TSocks5AuthResponse)) != sizeof(TSocks5AuthResponse)
             || s5AResp.VER != 0x05
             || s5AResp.METHOD != 0x00) {
-            Serial.printf("Socks5Auth failed response: VER=%d; METHOD=%d\n", s5AResp.VER, s5AResp.METHOD);
-            return false;
+            #ifdef TRACE_MESSAGES    
+                Serial.printf("Socks5Auth failed response: VER=%d; METHOD=%d\n", s5AResp.VER, s5AResp.METHOD);
+            #endif
+                return false;
         }
+    #ifdef TRACE_MESSAGES 
         Serial.printf("Socks5Auth success: VER=%d; METHOD=%d\n", s5AResp.VER, s5AResp.METHOD);
+    #endif
         return true;
     }
 
     int Socks5ConnectResponse() {
+    #ifdef TRACE_MESSAGES     
         Serial.println("Socks5 CONNECT RESPONSE");
+    #endif    
         struct Socks5ConnResponse {
             uint8_t VER;
             uint8_t REP;
@@ -175,20 +201,23 @@ protected:
             || s5CResp.VER != 0x05
             || s5CResp.REP != 0x00
             || s5CResp.ATYP != 0x01) {
-            Serial.printf("Socks5ConnectResponse failure: VAR=%d; REP=%d; ATYP=%d\n", s5CResp.VER, s5CResp.REP, s5CResp.ATYP);
-            return 0;
+            #ifdef TRACE_MESSAGES       
+                Serial.printf("Socks5ConnectResponse failure: VAR=%d; REP=%d; ATYP=%d\n", s5CResp.VER, s5CResp.REP, s5CResp.ATYP);
+            #endif
+                return 0;
         }
+    #ifdef TRACE_MESSAGES        
         Serial.printf("Socks5ConnectResponse success: VAR=%d; REP=%d; ATYP=%d\n", s5CResp.VER, s5CResp.REP, s5CResp.ATYP);
-        //IPAddress destIp { s5CResp.ADDR[0], s5CResp.ADDR[1], s5CResp.ADDR[2], s5CResp.ADDR[3] };
-        //uint16_t destPort = ntohs(s5CResp.PORT);
-
+    #endif
         return 1;
     }
 
     int Socks5ClientConnectWithRetry(const IPAddress& destIp, uint16_t destPort) {
+    #ifdef TRACE_MESSAGES    
         Serial.printf("\nSocks5ClientConnectWithRetry\n");
         Serial.printf("HOST: %s\n", destIp.toString().c_str());
         Serial.printf("PORT: %d\n", destPort);
+    #endif    
         int r=0; //retry counter
         while((!client.connect(destIp, destPort)) && (r < 30)){
             yield();
@@ -197,11 +226,16 @@ protected:
             r++;
         }
         if(r==30) {
+        #ifdef TRACE_MESSAGES        
             Serial.println("WiFi Client Connection failed");
+        #endif    
             return 0;
-        } else {
+        }
+    #ifdef TRACE_MESSAGES 
+        else {
             Serial.println("WiFi Client Connected to web");
         }
+    #endif
         return 1;
     }
 
@@ -216,11 +250,13 @@ protected:
         } __attribute__((packed)) s5CReq;
             s5CReq.ADDR = destIp;
             s5CReq.PORT = htons(destPort);
+    #ifdef TRACE_MESSAGES            
         Serial.println("Socks5ConnRequest:");
         for (size_t i = 0; i < sizeof(s5CReq); ++i) {
             Serial.printf("%d:0x%x(%d) ", i, ((uint8_t*)&s5CReq)[i], ((uint8_t*)&s5CReq)[i]);
         }
         Serial.println("");
+    #endif
         if (WriteAll(client, (uint8_t*)&s5CReq, sizeof(s5CReq)) != sizeof(s5CReq)
             || !WaitClient(client)) {
             return false;
@@ -233,7 +269,9 @@ protected:
         if (dnLen > 255) {
             return false;
         }
+    #ifdef TRACE_MESSAGES    
         Serial.printf("\nDN: %s; LEN: %d\n", destHost, dnLen);
+    #endif
         // Socks5 Connect Request
         struct Socks5ConnRequest {
             uint8_t VER  = 0x05;
@@ -246,20 +284,27 @@ protected:
         if (WriteAll(client, (uint8_t*)&s5CReq, sizeof(Socks5ConnRequest)) != sizeof(Socks5ConnRequest)) {
             return false;
         };
+    #ifdef TRACE_MESSAGES    
         for (uint8_t i = 0; i < s5CReq.DN_LEN; ++i) {
             Serial.printf("DN[%d]='%c'\n", i, destHost[i]);
         }
+    #endif    
         if (WriteAll(client, (uint8_t*)destHost, s5CReq.DN_LEN) != s5CReq.DN_LEN) {
+        #ifdef TRACE_MESSAGES    
             Serial.println("Socks5 bad send DN to socks5");
+        #endif    
             return false;
         };
         uint16_t port = htons(destPort);
+    #ifdef TRACE_MESSAGES    
         Serial.printf("PORT = %d\n", destPort);
-
+    #endif
         if (WriteAll(client, (uint8_t*)&port, 2) != 2) {
             return false;
         };
+    #ifdef TRACE_MESSAGES    
         Serial.printf("Send CONNECT REQUEST\n");
+    #endif    
         return true;
     }
 };
